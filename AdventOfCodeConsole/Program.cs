72c0e1e5ee20a1a78489e911e5643249ca7e0061
Brady Kelly <brady@bradykelly.net>
2021-12-03 16:11:32 +0200

    Refactoring

--- AdventOfCodeConsole/Program.cs
+++ AdventOfCodeConsole/Program.cs
@@ -3,9 +3,9 @@ using AdventOfCodeConsole.Benchmarks;
 using BenchmarkDotNet.Running;
 
 
-//var summary = BenchmarkRunner.Run<Day2Bench>();
-var runner = new Day3Bench();
-await runner.Setup();
-runner.Part1();
-runner.Part2();
-runner.ShowResults();
+var summary = BenchmarkRunner.Run<Day2Bench>();
+//var runner = new Day3Bench();
+//await runner.Setup();
+//runner.Part1();
+//runner.Part2();
+//runner.ShowResults();
--- AdventOfCodeConsole/Puzzles/Day3.cs
+++ AdventOfCodeConsole/Puzzles/Day3.cs
@@ -1,45 +1,55 @@
-﻿namespace AdventOfCodeConsole.Puzzles;
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCodeConsole.Puzzles;
 
 internal static class Day3
 {
-    private static char MostCommonBit(int pos, string[] binaries)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static char? MostCommonBit(int pos, List<string> binaries)
     {
         var count0 = 0;
         var count1 = 0;
-        for (var i = 0; i < binaries.Length; i++)
+        foreach (var bin in binaries)
         {
-            if (binaries[i][pos] == '0')
-                count0 += 1;
-            else
+            switch (bin[pos])
             {
-                count1 += 1;
+                case '0':
+                    count0 += 1;
+                    break;
+                default:
+                    count1 += 1;
+                    break;
             }
         }
 
-        return count1 > count0 ? '1' : '0';
+        return count1 == count0 ? null : count1 > count0 ? '1' : '0';
     }
 
-    private static char LeastCommonBit(int pos, string[] binaries)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static char? LeastCommonBit(int pos, List<string> binaries)
     {
         var count0 = 0;
         var count1 = 0;
-        for (var i = 0; i < binaries.Length; i++)
+        foreach (var bin in binaries)
         {
-            if (binaries[i][pos] == '0')
-                count0 += 1;
-            else
+            switch (bin[pos])
             {
-                count1 += 1;
+                case '0':
+                    count0 += 1;
+                    break;
+                default:
+                    count1 += 1;
+                    break;
             }
         }
 
-        return count1 > count0 ? '0' : '1';
+        return count1 == count0 ? null : count1 > count0 ? '0' : '1';
     }
 
     public static int Part1(string input)
     {
         var binary = "";
-        var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
         for (var pos = 0; pos < lines[0].Length; pos++)
         {
             var mostCommon = MostCommonBit(pos, lines);
@@ -47,7 +57,7 @@ internal static class Day3
         }
 
         var gamma = Convert.ToInt32(binary, 2);
-        
+
         binary = "";
         for (var pos = 0; pos < lines[0].Length; pos++)
         {
@@ -62,6 +72,32 @@ internal static class Day3
 
     public static int Part2(string input)
     {
-        return 0;
+        var binaries = input.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
+        var binaryLength = binaries[0].Length;
+
+        var oxygenNumbers = binaries;
+        for (var pos = 0; pos < binaryLength; pos++)
+        {
+            var mostCommon = MostCommonBit(pos, oxygenNumbers);
+            oxygenNumbers = oxygenNumbers.Count switch
+            {
+                > 1 => oxygenNumbers.Where(bin => bin[pos] == (mostCommon ?? '1')).ToList(),
+                _ => oxygenNumbers
+            };
+        }
+
+        var co2Numbers = binaries;
+        for (var pos = 0; pos < binaryLength; pos++)
+        {
+            var leastCommon = LeastCommonBit(pos, co2Numbers);
+            co2Numbers = co2Numbers.Count switch
+            {
+                > 1 => co2Numbers.Where(bin => bin[pos] == (leastCommon ?? '0')).ToList(),
+                _ => co2Numbers
+            };
+        }
+
+        var support = Convert.ToInt32(oxygenNumbers[0], 2) * Convert.ToInt32(co2Numbers[0], 2);
+        return support;
     }
 }
