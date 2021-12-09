aa2d7e9d6397d910557b4d9606f7ca69fe6e0224
Brady Kelly <brady@bradykelly.net>
2021-12-09 19:08:44 +0200

    Before LINQ

--- AdventOfCodeConsole/Puzzles/2021/Day9.cs
+++ AdventOfCodeConsole/Puzzles/2021/Day9.cs
@@ -5,32 +5,18 @@ namespace AdventOfCodeConsole.Puzzles._2021;
 
 public class Day9 : IDay
 {
-    public long Part1(string input)
+    private static List<(int y, int x)> GetLowPoints(int[,] heatMap)
     {
-        var bigTotal = 0;
+        var rowCount = heatMap.GetLength(0);
+        var colCount = heatMap.GetLength(1);
 
-        var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
-        var heatMap = new int[lines.Length, lines[0].Length];
-
-        for (var y = 0; y < lines.Length; y++)
+        var lowPoints = new List<(int y, int x)>();
+        for (var y = 0; y < rowCount; y++)
         {
-            var line = lines[y];
-            for (var x = 0; x < line.Length; x++)
+            for (var x = 0; x < colCount; x++)
             {
-                heatMap[y, x] = line[x];
-            }
-        }
-
-        var rows = heatMap.GetLength(0);
-        var cols = heatMap.GetLength(1);
-
-        var lowPoints = new List<int>();
-        for (var y = 0; y < rows; y++)
-        {
-            for (var x = 0; x < cols; x++)
-            {
-                var height = heatMap[y, x] - '0';
-                var adjacents = EnumerableMatrixFunctions.AdjacentElementsOrthogonal(heatMap, y, x).Select(a => a - '0');
+                var height = heatMap[y, x];
+                var adjacents = EnumerableMatrixFunctions.AdjacentElementsOrthogonal(heatMap, y, x);
 
                 var isLow = true;
                 foreach (var adj in adjacents)
@@ -44,12 +30,35 @@ public class Day9 : IDay
 
                 if (isLow)
                 {
-                    bigTotal += height + 1;
-                    //lowPoints.Add(height);
+                    lowPoints.Add((y, x));
                 }
             }
         }
 
+        return lowPoints;
+    }
+
+    public long Part1(string input)
+    {
+        int bigTotal = 0;
+        var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        var heatMap = new int[lines.Length, lines[0].Length];
+
+        for (var y = 0; y < lines.Length; y++)
+        {
+            var line = lines[y];
+            for (var x = 0; x < line.Length; x++)
+            {
+                heatMap[y, x] = line[x] - '0';
+            }
+        }
+
+        var lows = GetLowPoints(heatMap);
+
+        foreach (var low in lows)
+        {
+            bigTotal += heatMap[low.y, low.x] + 1;
+        }
         return bigTotal;
     }
 
