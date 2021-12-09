ce5af7b912e601207d1a4e71afb9ed3bc855baa3
Brady Kelly <brady@bradykelly.net>
2021-12-09 23:52:57 +0200

    Changed for debugging

--- AdventOfCodeConsole/Tools/GridMethods.cs
+++ AdventOfCodeConsole/Tools/GridMethods.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,24 @@ namespace AdventOfCodeConsole.Tools
 {
     public class GridMethods
     {
-        public static IEnumerable<T> AdjacentElements<T>(T[,] array, int row, int column)
+        public static IEnumerable<Point> AdjacentElements(int[,] array, int row, int column)
         {
             int rows = array.GetLength(0);
             int columns = array.GetLength(1);
 
             for (int y = row - 1; y <= row + 1; y++)
             for (int x = column - 1; x <= column + 1; x++)
-                if (x >= 0 && y >= 0 && x < columns && y < rows) 
-                    if (!(y == row && x == column) 
-                        && !(y == row - 1 && x == column - 1) 
-                        && !(y == row + 1 && x == column + 1) 
+                if (x >= 0 && y >= 0 && x < columns && y < rows)
+                    if (!(y == row && x == column)
+                        && !(y == row - 1 && x == column - 1)
+                        && !(y == row + 1 && x == column + 1)
                         && !(y == row - 1 && x == column + 1)
                         && !(y == row + 1 && x == column - 1))
-                        yield return array[y, x];
+                    {
+                        if (y == 0)
+                            Debug.Fail("Y is 6");
+                        yield return new Point(y, x);
+                    }
         }
 
         public static IEnumerable<T> NeighbouringElements<T>(T[,] array, int row, int column)
