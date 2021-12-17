c993ca2202db16023b072e0209d00c4e86be0ee3
Brady Kelly <brady@bradykelly.net>
2021-12-17 11:37:24 +0200

    Optimized day 1

--- AdventOfCodeConsole/Program.cs
+++ AdventOfCodeConsole/Program.cs
@@ -1,8 +1,8 @@
-﻿using AdventOfCodeConsole.Runners._2020;
+﻿using AdventOfCodeConsole.Runners._2021;
 using BenchmarkDotNet.Running;
 
 #if !DEBUG
-    BenchmarkRunner.Run<Day4Runner>();
+    BenchmarkRunner.Run<Day1Runner>();
 #else
-    await new Day4Runner().Run();
+    await new Day1Runner().Run();
 #endif
--- AdventOfCodeConsole/Puzzles/2021/Day1.cs
+++ AdventOfCodeConsole/Puzzles/2021/Day1.cs
@@ -1,38 +1,53 @@
-﻿namespace AdventOfCodeConsole.Puzzles._2021
+﻿namespace AdventOfCodeConsole.Puzzles._2021;
+
+public class Day1 : IDay
 {
-    public class Day1: IDay
+    private static int[] GetIntsFromInput(string input)
     {
-        private static int[] GetIntsFromInput(string? input)
+        var ints = new List<int>();
+        if (ints.Count == 0)
         {
-            return input is null ? Array.Empty<int>() : input
-                .Split('\n', StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToArray();
-        }
-
-        public long Part1(string? input)
-        {
-            var depths = GetIntsFromInput(input);
-            return depths
-                .Select((n, i) => i != 0 && depths[i - 1] < n)
-                .Count(b => b);
-        }
-
-        public long Part2(string? input)
-        {
-            var depths = GetIntsFromInput(input);
-
-            var count = 0;
-            for (var i = 2; i < depths.Length - 1; i++)
+            var lines = input
+                .Split('\n');
+            foreach (var line in lines)
             {
-                // a + b + c  >  b + c + d is the same as a > d
-                if (depths[i + 1] > depths[i - 2])
+                if (line != string.Empty)
                 {
-                    count++;
+                    ints.Add(int.Parse(line));
                 }
             }
-
-            return count;
         }
+
+        return ints.ToArray();
+    }
+
+    public long Part1(string input)
+    {
+        var depths = GetIntsFromInput(input);
+        var increases = 0;
+        for (var i = 1; i < depths.Length; i++)
+        {
+            if (depths[i - 1] < depths[i])
+                increases++;
+        }
+
+        return increases;
+    }
+
+    public long Part2(string input)
+    {
+        var depths = GetIntsFromInput(input);
+
+        var count = 0;
+        for (var i = 2; i < depths.Length - 1; i++)
+        {
+            // (a + b + c'  >  (b + c + d) is the same as a > d
+            if (depths[i + 1] > depths[i - 2])
+            {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
