893689f06954c4ac5adb1aca63e5e26abbe03d2e
Brady Kelly <brady@bradykelly.net>
2021-12-17 11:11:25 +0200

    2020 Day 4 Part 1 and 2

--- AdventOfCodeConsole/Puzzles/2020/Day4.cs
+++ AdventOfCodeConsole/Puzzles/2020/Day4.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCodeConsole.Puzzles._2020;
 
@@ -80,61 +81,67 @@ public class Day4 : IDay
             if (required.AllPresent())
             {
                 var valid = true;
-                //foreach (var field in fieldSplit)
-                //{
+                foreach (var field in docsSplit)
+                {
+                    var fieldSplit = field.Split(':');
+                    var name = fieldSplit[0];
+                    var value = fieldSplit.Length > 1 ? field.Split(':')[1] : "";
+                    switch (name)
+                    {
+                        case "byr":
+                            if (!int.TryParse(value, out var yearb) || yearb is < 1920 or > 2002) valid = false;
+                            break;
 
-                //    var value = field.Split(':')[1];
-                //    switch (field.Split(':')[0])
-                //    {
-                //        case "byr":
-                //            if (!int.TryParse(value, out var yearb) || yearb is < 1920 or > 2002) valid = true;
-                //            break;
+                        case "iyr":
+                            if (!int.TryParse(value, out var yeari) || yeari is < 2010 or > 2020) valid = false;
+                            break;
 
-                //        case "iyr":
-                //            if (!int.TryParse(value, out var yeari) || yeari is < 2010 or > 2020) valid = false;
-                //            break;
+                        case "eyr":
+                            if (!int.TryParse(value, out var yeare) || yeare is < 2020 or > 2030) valid = false;
+                            break;
 
-                //        case "eyr":
-                //            if (!int.TryParse(value, out var yeare) || yeare is < 2020 or > 2030) valid = false;
-                //            break;
+                        case "hgt":
+                            var rxHg = new Regex(@"^(\d{3})(cm)$|^(\d{2})(in)$");
+                            var mxHg = rxHg.Match(value);
+                            if (!mxHg.Success || mxHg.Groups.Count < 3)
+                            {
+                                valid = false;
+                                break;
+                            }
 
-                //        case "hgt":
-                //            var rxHg = new Regex(@"(^\d{3})(cm)\b|(^\d{2})(in)");
-                //            var mxHg = rxHg.Match(value);
-                //            if (!mxHg.Success || mxHg.Groups.Count != 2)
-                //            {
-                //                valid = false;
-                //                break;
-                //            }
+                            if (mxHg.Groups[2].Value == "cm" && int.Parse(mxHg.Groups[1].Value) is < 150 or > 193) valid = false;
+                            if (mxHg.Groups[2].Value == "in" && int.Parse(mxHg.Groups[1].Value) is < 59 or > 76) valid = false;
+                            break;
 
-                //            if (mxHg.Groups[1].Value == "cm" && int.Parse(mxHg.Groups[0].Value) is < 150 or > 193) valid = false;
-                //            if (mxHg.Groups[1].Value == "in" && int.Parse(mxHg.Groups[0].Value) is < 59 or > 76) valid = false;
-                //            valid = false;
-                //            break;
+                        case "hcl":
+                            var rxCl = new Regex(@"^#[0-9a-f]{6}$");
+                            if (!rxCl.Match(value).Success)
+                                valid = false;
+                            break;
 
-                //        case "hcl":
-                //            var rxCl = new Regex(@"^#[0-9a-f]{6}$");
-                //            valid = rxCl.Match(value).Success;
-                //            break;
+                        case "ecl":
+                            const string set = " amb blu brn gry grn hzl oth ";
+                            if (!set.Contains($" {value} ")) valid = false;
+                            break;
 
-                //        case "ecl":
-                //            const string set = " amb blu brn gry grn hzl oth ";
-                //            if (!set.Contains($" {value} ")) valid = false;
-                //            break;
+                        case "pid":
+                            var rxPd = new Regex(@"^\d{9}$");
+                            if (!rxPd.Match(value).Success)
+                                valid = false;
+                            break;
 
-                //        case "pid":
-                //            var rxPd = new Regex(@"^\d{9}$");
-                //            valid = rxPd.Match(value).Success;
-                //            break;
+                        case "cid":
+                            break;
+                    }
+                }
 
-                //        case "cid":
-                //            break;
-                //    }
-
-                //}
-                if (valid) validDocs++;
-                required.Reset();
+                if (valid)
+                {
+                    Debug.Print(doc);
+                    validDocs++;
+                }
             }
+            required.Reset();
         }
 
         return validDocs;
